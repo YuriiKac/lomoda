db1989f0261708915b3d07c25f2b28a1487936a6
YuriiKac <84316108+YuriiKac@users.noreply.github.com>
2021-07-07 19:20:27 +0300

    Query from the database

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const headerCityButton = document.querySelector(".header__city-button");
 
+let hash = location.hash.substring(1);
+
 headerCityButton.textContent =
   localStorage.getItem("lomoda-location") || "Ваш город?";
 
@@ -61,3 +63,101 @@ const anableScroll = () => {
     top: document.body.dbScrollY,
   });
 };
+
+/* 3. query from the database */
+
+const getData = async () => {
+  const data = await fetch("db.json");
+  if (data.ok) {
+    return data.json();
+  } else {
+    throw new Error(
+      `Данные небыли получены, ошибка ${data.status} ${data.statusText}`
+    );
+  }
+};
+
+const getGoods = (callback, value) => {
+  getData()
+    .then((data) => {
+      if (value) {
+        callback(data.filter((item) => item.category === value));
+      } else {
+        callback(data);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+try {
+  const goodsList = document.querySelector(".goods__list");
+
+  if (!goodsList) {
+    throw "This is not a goods page";
+  }
+
+  const createCard = ({ id, preview, cost, brand, name, sizes }) => {
+    const li = document.createElement("li");
+
+    li.classList.add("goods__item");
+
+    li.innerHTML = `
+       <article class="good">
+           <a class="good__link-img" href="card-good.html#${id}">
+               <img class="good__img" src="goods-image/${preview}" alt="">
+           </a>
+           <div class="good__description">
+                <p class="good__price">${cost} &#8381;</p>
+                <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                ${
+                  sizes
+                    ? `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(
+                        " "
+                      )}</span>
+                    </p>
+                  `
+                    : ""
+                }
+                
+                <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+           </div>
+       </article>
+    `;
+
+    return li;
+  };
+
+  const renderGoodsList = (data) => {
+    goodsList.textContent = "";
+
+    data.forEach((item) => {
+      const card = createCard(item);
+      goodsList.append(card);
+    });
+  };
+
+  window.addEventListener("hashchange", () => {
+    hash = location.hash.substring(1);
+    getGoods(renderGoodsList, hash);
+    const pageTitle = document.querySelector(".goods__title");
+    switch (hash) {
+      case "men":
+        pageTitle.textContent = "Мужчинам";
+        break;
+      case "women":
+        pageTitle.textContent = "Женщинам";
+        break;
+      case "kids":
+        pageTitle.textContent = "Детям";
+        break;
+      default:
+        pageTitle.textContent = "";
+    }
+  });
+
+  getGoods(renderGoodsList, hash);
+} catch (err) {
+  console.log(err);
+}
